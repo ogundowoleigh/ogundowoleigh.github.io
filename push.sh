cbfb7b3b14ecab62abdbb6fdff76cce7c30ec6ba
Simisola Ogundowole <ogundowoleigh@yahoo.com>
2021-01-03 10:03:14 -0500

    Add Google Analytics code

--- push.sh
+++ push.sh
@@ -1,3 +1,7 @@
 git add .
-git commit -m "Updated files"
+echo "Staged recent changes."
+echo "Now, enter commit message..."
+read commit_message
+git commit -m "$commit_message"
 git push
+echo "Commit successfully pushed!"
